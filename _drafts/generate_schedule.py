3839145a4e7522350d66e9342de8f7fe22e8cf4b
aylvisaker <ajylvisaker@gmail.com>
2016-12-16 18:29:53 -0600

    schedule

--- _drafts/generate_schedule.py
+++ _drafts/generate_schedule.py
@@ -11,15 +11,12 @@ discussions = [item[1] for item in source]
 
 schedule_markup.write('---\nlayout: post\ntitle: "Course Schedule"\n')
 schedule_markup.write('categories: [syllabus]\ntags: [syllabus]\n')
-schedule_markup.write('description: MC-MATH-152\n---\n\n')
+schedule_markup.write('description: MC-MATH-301\n---\n\n')
 schedule_markup.write('|Week |Monday|Tuesday|Wednesday|Friday|\n')
 schedule_markup.write('|:---:|:---: |:---:  |:---:    |:---: |\n')
 iterator = 0
-lab_iterator = 0
 for week in range(len(academic_calendar)):
     row = '|' + str(week + 1) + '|'
-    if academic_calendar[week][3]:
-        lab_iterator += 1
     for day in [0,1,2,4]:
         current_day = start + datetime.timedelta(days = 7 * week + day)
         if not academic_calendar[week][day]:
@@ -30,4 +27,3 @@ for week in range(len(academic_calendar)):
     schedule_markup.write(row + '\n')
 
 print(iterator)
-print(lab_iterator)
---
+++ _posts/2016-1-15-schedule.md
@@ -0,0 +1,26 @@
+---
+layout: post
+title: "Course Schedule"
+categories: [syllabus]
+tags: [syllabus]
+description: MC-MATH-301
+---
+
+|Week |Monday|Tuesday|Wednesday|Friday|
+|:---:|:---: |:---:  |:---:    |:---: |
+|1|Syllabus|1.1-3|1.4-5|1.6-7|
+|2|2.1|2.2|2.3|2.4-5|
+|3|**Exam**|3.1|3.2-3|3.4-5|
+|4|3.6|3.7-8|3.9|3.10-12|
+|5|3.13|3.14|**Exam**|4.1|
+|6|4.2|4.3|4.4-5|4.6-7|
+|7|5.1|5.2-3|5.4-6|NO CLASS|
+|8|NO CLASS|NO CLASS|NO CLASS|NO CLASS|
+|9|**Exam**|6.1|6.1|6.2-3|
+|10|6.4-5|7.1|7.2-3|7.4-5|
+|11|7.6|7.7|7.7|**Exam**|
+|12|8.1|8.2|8.3-4|11.1|
+|13|11.2|11.2|11.3-4|NO CLASS|
+|14|NO CLASS|11.5|11.6|11.6|
+|15|Flex|NO CLASS|Flex|Flex|
+|16|Flex|Flex|Flex|NO CLASS|
